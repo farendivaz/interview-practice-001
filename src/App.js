fe7460057b6f7107c541b9ba563f3dc77c9e40f5
farendivaz <farendivaz@gmail.com>
2022-06-29 06:31:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+const API_URL = "https://pokeapi.co/api/v2/ability";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async (search) => {
+    const response = await fetch(`${API_URL}/${search}`);
+    const data = await response.json();
+    setData(data.pokemon);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData(search);
+  };
+  console.log(search);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="Search ability"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>submit</button>
+      </form>
+      {/* {data.effect_entries[0].short_effect} */}
+      {data.map((poke) => {
+        return <h1>{poke.pokemon.name}</h1>;
+      })}
+    </>
   );
 }
 
